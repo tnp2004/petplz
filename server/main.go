1d6d4a28b1de322b8d4283238586583978a3786f
tnp2004 <iceandiceisice2004@gmail.com>
2023-07-20 18:58:19 +0700

    connecting with Mongodb

--- server/main.go
+++ server/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/tnp2004/Renter/repository"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
+
+	loadDotEnv()
+
+	dbUri := os.Getenv("MONGODB_URI")
+	if dbUri == "" {
+		log.Fatal("mongodb uri is empty")
+	}
+
+	dbClient := initDatabase(dbUri)
+	defer func() {
+		if err := dbClient.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	accountRepositoryDb := repository.NewAccountRepositoryDB(dbClient)
+	accountRepositoryDb.GetAccount("64b91d161ee24217a99b9d60")
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -10,4 +38,19 @@ func main() {
 	})
 
 	app.Listen(":3000")
+}
+
+func initDatabase(uri string) *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
+
+func loadDotEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file found")
+	}
 }
--- server/repository/account_db.go
+++ server/repository/account_db.go
@@ -1,1 +1,44 @@
 package repository
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type accountRepositoryDB struct {
+	db *mongo.Client
+}
+
+func NewAccountRepositoryDB(db *mongo.Client) AccountRepository {
+	return accountRepositoryDB{db}
+}
+
+func (r accountRepositoryDB) Create(account Account) (*Account, error) {
+	return nil, nil
+}
+func (r accountRepositoryDB) GetAccount(id string) (*Account, error) {
+	objectId, _ := primitive.ObjectIDFromHex(id)
+
+	coll := r.db.Database("Renter").Collection("Account")
+	var result bson.M
+	err := coll.FindOne(context.TODO(), bson.D{{Key: "_id", Value: objectId}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the title %s\n", id)
+		return nil, err
+	}
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+
+	return nil, nil
+}

7679f48caea153a47f2183c4235fb7ea39d3614e
tnp2004 <iceandiceisice2004@gmail.com>
2023-07-24 19:07:02 +0700

    add sign in page

---
+++ client/src/routes/signin/+page.svelte
@@ -0,0 +1,15 @@
+<div class="flex h-screen">
+	<div class="m-auto drop-shadow-xl bg-white rounded p-2 text-slate-700">
+		<h1 class="text-2xl font-bold">Sign in</h1>
+		<div class="my-5">
+			<div class="my-2 w-72">
+				<input class="border-2 rounded-sm w-full px-1 my-2 h-9" placeholder="email" type="text" />
+				<input class="border-2 rounded-sm w-full px-1 my-2 h-9" placeholder="password" type="text" />
+			</div>
+			<button class="bg-emerald-200 font-bold w-full rounded-sm p-1 h-9">SIGN IN</button>
+		</div>
+		<div class="text-center text-slate-400">
+			<a href="/signup">Create Account ?</a>
+		</div>
+	</div>
+</div>

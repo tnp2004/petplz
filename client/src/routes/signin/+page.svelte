0707f814f5be5a225e1d41777b04267893b3c84b
tnp2004 <iceandiceisice2004@gmail.com>
2023-07-28 21:36:53 +0700

    decorate sign in/up form

--- client/src/routes/signin/+page.svelte
+++ client/src/routes/signin/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex h-screen">
 	<div class="m-auto drop-shadow-xl bg-white rounded py-2 px-5 text-slate-700">
-		<h1 class="text-2xl font-bold">Sign in</h1>
+		<h1 class="text-2xl font-bold bg-slate-200 w-fit px-3 py-1 rounded">Sign in</h1>
 		<div class="my-5">
 			<div class="my-2 w-72">
 				<label for="email">
@@ -17,10 +17,10 @@
                     <input bind:value={password} class="border-2 rounded-sm w-full px-1 my-2 h-9" type="text"/>
                 </label>
 			</div>
-			<button class="bg-emerald-200 font-bold w-full rounded-sm p-1 h-9">SIGN IN</button>
+			<button class="font-bold w-full btn btn-success rounded">SIGN IN</button>
 		</div>
-		<div class="text-center text-slate-400">
-			<a href="/signup">Don't have an account yet ?</a>
+		<div class="text-center">
+			<a class="text-slate-700/50" href="/signup">don't have an account ?</a>
 		</div>
 	</div>
 </div>
--- client/src/routes/signup/+page.svelte
+++ client/src/routes/signup/+page.svelte
@@ -1,59 +1,94 @@
 <script lang="ts">
-    type Gender = "male" | "female" | "other";
+	type Gender = 'male' | 'female' | 'other';
 
-    let username: string;
-    let email: string;
-    let password: string;
-    let confirmPassword: string;
-    let age: string;
+	let username: string;
+	let email: string;
+	let password: string;
+	let confirmPassword: string;
+	let age: string;
 	let gender: Gender;
-	
 </script>
 
 <div class="flex h-screen">
 	<div class="m-auto drop-shadow-xl bg-white rounded py-2 px-5 text-slate-700">
-		<h1 class="text-2xl font-bold">Sign up</h1>
+		<h1 class="text-2xl font-bold bg-slate-200 w-fit px-3 py-1 rounded">Sign up</h1>
 		<div class="my-5">
 			<div class="my-2 w-72">
-                <label for="username">
-                    username
-                    <input bind:value={username} class="border-2 rounded-sm w-full px-1 my-2 h-9" type="text" />
-                </label>
-                <label for="email">
-                    Email
-                    <input bind:value={email} class="border-2 rounded-sm w-full px-1 my-2 h-9" type="text" />
-                </label>
-                <label for="password">
-                    Password
-                    <input bind:value={password} class="border-2 rounded-sm w-full px-1 my-2 h-9" type="text"/>
-                </label>
-                <label for="confirm-password">
-                    Confirm Password
-                    <input bind:value={confirmPassword} class="border-2 rounded-sm w-full px-1 my-2 h-9" type="text"/>
-                </label>
-                <label for="age">
-                    Age
-                    <input bind:value={age} class="border-2 rounded-sm w-full my-2 px-1 h-9" type="text" />
-                </label>
+				<label for="username">
+					Username
+					<input
+						bind:value={username}
+						class="border-2 rounded-sm w-full px-1 my-2 h-9"
+						type="text"
+					/>
+				</label>
+				<label for="email">
+					Email
+					<input bind:value={email} class="border-2 rounded-sm w-full px-1 my-2 h-9" type="text" />
+				</label>
+				<label for="password">
+					Password
+					<input
+						bind:value={password}
+						class="border-2 rounded-sm w-full px-1 my-2 h-9"
+						type="text"
+					/>
+				</label>
+				<label for="confirm-password">
+					Confirm Password
+					<input
+						bind:value={confirmPassword}
+						class="border-2 rounded-sm w-full px-1 my-2 h-9"
+						type="text"
+					/>
+				</label>
+				<label for="age">
+					Age
+					<input bind:value={age} class="border-2 rounded-sm w-full my-2 px-1 h-9" type="text" />
+				</label>
 				<div class="flex gap-2 my-2 justify-between">
-						<button on:click={() => (gender = 'male')} class="rounded w-full border-2 p-1">
-							<input type="radio" value="male" checked={gender == 'male'} />
-							Male
-						</button>
-						<button on:click={() => (gender = 'female')} class="rounded w-full border-2 p-1">
-							<input type="radio" value="female" checked={gender == 'female'} />
-							Female
-						</button>
-						<button on:click={() => (gender = 'other')} class="rounded w-full border-2 p-1">
-							<input type="radio" value="other" checked={gender == 'other'} />
-							Other
-						</button>
+					<button
+						on:click={() => (gender = 'male')}
+						class="rounded w-full border-2 p-1 flex justify-around"
+					>
+						<input
+							class="radio radio-success w-5 h-5 my-auto mx-1"
+							type="radio"
+							value="male"
+							checked={gender == 'male'}
+						/>
+						<span class="w-full">Male</span>
+					</button>
+					<button
+						on:click={() => (gender = 'female')}
+						class="rounded w-full border-2 p-1 flex justify-around"
+					>
+						<input
+							class="radio radio-success w-5 h-5 my-auto mx-1"
+							type="radio"
+							value="female"
+							checked={gender == 'female'}
+						/>
+						<span class="w-full">Female</span>
+					</button>
+					<button
+						on:click={() => (gender = 'other')}
+						class="rounded w-full border-2 p-1 flex justify-around"
+					>
+						<input
+							class="radio radio-success w-5 h-5 my-auto mx-1"
+							type="radio"
+							value="other"
+							checked={gender == 'other'}
+						/>
+						<span class="w-full">Other</span>
+					</button>
 				</div>
 			</div>
-			<button class="bg-emerald-200 font-bold w-full rounded-sm p-1 h-9">SIGN UP</button>
+			<button class="font-bold w-full btn btn-success rounded">SIGN UP</button>
 		</div>
 		<div class="text-center text-slate-400">
-			<a href="/signin">already have an account ?</a>
+			<a class="text-slate-700/50" href="/signin">already have an account ?</a>
 		</div>
 	</div>
 </div>

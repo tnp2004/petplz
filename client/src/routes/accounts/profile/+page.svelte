1e112845e0c8a485815140e339469cac2102a882
tnp2004 <iceandiceisice2004@gmail.com>
2023-08-04 22:32:41 +0700

    update information in profile page

--- client/src/routes/accounts/profile/+page.svelte
+++ client/src/routes/accounts/profile/+page.svelte
@@ -1,33 +1,68 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-    import { authenticated } from "../../../stores/auth.ts"
+	import { authenticated } from '../../../stores/auth';
 
-    interface Account {
-        accountId: string,
-        username: string,
-        email: string,
-        gender: "male" | "female" | "other",
-        age: number,
-        money: number,
-        image_uri: string,
-    }
+	interface Account {
+		accountId: string;
+		username: string;
+		email: string;
+		gender: 'male' | 'female' | 'other';
+		age: number;
+		money: number;
+		image_uri: string;
+	}
 
-    let accountData: Account;
+	let accountData: Account;
 
-    onMount(async () => {
-			const response = await fetch('http://localhost:3000/api/accounts', {
-				headers: { 'Content-Type': 'application/json' },
-				credentials: 'include'
-			});
+	onMount(async () => {
+		const response = await fetch('http://localhost:3000/api/accounts', {
+			headers: { 'Content-Type': 'application/json' },
+			credentials: 'include'
+		});
 
-            const resOkay = response.ok
-            authenticated.set(resOkay);
-            if (response.ok) {
-                const data = await response.json();
-                accountData = data
-            }
+		const resOkay = response.ok;
+		authenticated.set(resOkay);
+		if (response.ok) {
+			const data = await response.json();
+			accountData = data;
+		}
 	});
 </script>
+
 {#if accountData}
-    {JSON.stringify(accountData)}
+	<div class="w-fit px-5 mx-auto my-10">
+        <div class="text-xl my-20 w-56">
+            <div class="text-center my-3">
+                <label class="font-bold text-4xl" for="profile">Profile</label>
+            </div>
+            <div class="my-3">
+                <img class="rounded-full" src={accountData.image_uri} alt="profile-image" />
+            </div>
+            <div class="flex justify-between">
+                <label class="font-bold w-1/2" for="username">Username</label>
+                <div class="w-1/2">
+                    {accountData.username}
+                </div>
+            </div>
+            <div class="flex justify-between">
+                <label class="font-bold w-1/2" for="email">Email</label>
+                <div class="w-1/2">
+                    {accountData.email}
+                </div>
+            </div>
+            <div class="flex justify-between">
+                <label class="font-bold w-1/2" for="gender">Gender</label>
+                <div class="w-1/2">
+                    {accountData.gender}
+                </div>
+            </div>
+            <div class="flex justify-between">
+                <label class="font-bold w-1/2" for="age">Age</label>
+                <div class="w-1/2">
+                    {accountData.age}
+                </div>
+            </div>
+           
+        </div>
+    </div>
 {/if}
